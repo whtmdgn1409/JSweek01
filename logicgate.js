fd26d2734f4f9101e9d0d1c1285d13801a6e968c
Eunwoo <dmsdn960@gmail.com>
2019-11-01 17:43:02 +0900

    전가산기 함수 구현

--- logicgate.js
+++ logicgate.js
@@ -25,6 +25,10 @@ function halfAdder(a, b) {
   return [xor(a, b), and(a, b)];
 }
 
+function fullAdder(a, b, c) {
+  return [xor(xor(a, b), c), or(and(a, b), and(c, xor(a, b)))];
+}
+
 test.truthTable.and.forEach(([a, b, answer]) => test.assert(and(a, b), answer));
 
 test.truthTable.or.forEach(([a, b, answer]) => test.assert(or(a, b), answer));
@@ -35,4 +39,8 @@ test.truthTable.xor.forEach(([a, b, answer]) => test.assert(xor(a, b), answer));
 
 test.truthTable.halfAdder.forEach(([a, b, s, c]) =>
   test.assert(halfAdder(a, b), [s, c])
+);
+
+test.truthTable.fullAdder.forEach(([a, b, x, s, c]) =>
+  test.assert(fullAdder(a, b, x), [s, c])
 );
