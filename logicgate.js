476d04ccfbe08dae52a255be91be3d5c46719a78
Eunwoo <dmsdn960@gmail.com>
2019-11-01 17:29:33 +0900

    반가산기 함수 구현

--- logicgate.js
+++ logicgate.js
@@ -17,8 +17,22 @@ function not(a) {
   return 1;
 }
 
+function xor(a, b) {
+  return and(not(and(a, b)), or(a, b));
+}
+
+function halfAdder(a, b) {
+  return [xor(a, b), and(a, b)];
+}
+
 test.truthTable.and.forEach(([a, b, answer]) => test.assert(and(a, b), answer));
 
 test.truthTable.or.forEach(([a, b, answer]) => test.assert(or(a, b), answer));
 
 test.truthTable.not.forEach(([a, answer]) => test.assert(not(a), answer));
+
+test.truthTable.xor.forEach(([a, b, answer]) => test.assert(xor(a, b), answer));
+
+test.truthTable.halfAdder.forEach(([a, b, s, c]) =>
+  test.assert(halfAdder(a, b), [s, c])
+);
--- test.js
+++ test.js
@@ -1,5 +1,11 @@
 function assert(expect, answer) {
-  if (expect == answer) {
+  if (typeof expect === "object") {
+    if (expect.length === answer.length) {
+      if (answer.every((ans, index) => ans == expect[index])) {
+        console.log("\033[32m통과\033[0m");
+      }
+    }
+  } else if (expect == answer) {
     console.log("\033[32m통과\033[0m");
   } else {
     console.log("\033[31m실패\033[0m");
@@ -8,7 +14,9 @@ function assert(expect, answer) {
 const truthTable = {
   and: [[0, 0, 0], [0, 1, 0], [1, 0, 0], [1, 1, 1]],
   or: [[0, 0, 0], [0, 1, 1], [1, 0, 1], [1, 1, 1]],
-  not: [[0, 1], [1, 0]]
+  not: [[0, 1], [1, 0]],
+  xor: [[0, 0, 0], [0, 1, 1], [1, 0, 1], [1, 1, 0]],
+  halfAdder: [[0, 0, 0, 0], [0, 1, 1, 0], [1, 0, 1, 0], [1, 1, 0, 1]]
 };
 
 module.exports = {
