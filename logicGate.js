a6ef93c42eaa416ee1e237501a6b76c5d90586be
pongsoyun <thdbstjdud@gmail.com>
2019-11-05 00:02:43 +0900

    seperate jsStyle, NOT jsStyle

--- logicGate.js
+++ logicGate.js
@@ -1,71 +1,22 @@
-// Logic Gate
-function andFunc2(a, b) {
-	if(a){ 
-		if(b){ 
-			return true; // a&&b return a
-		}else{
-			return false; // a&&b return b
-		}
-	}else {
-		return false; 
-	}
-}
-
+/// Logic Gate
 function andFunc(a,b){
     return (a ? (b? true : false) : false);
 }
 
-///////////////////////////////////////////
-
-function orFunc2(a, b) {
-	if(a){
-		return true;
-	}else {
-		if(b){
-			return true;
-		}else {
-			return false;		
-		}
-	}
-}
 
 function orFunc(a,b){
     return (a ? true : (b ? true : false));
 }
 
-///////////////////////////////////////////
-
-function notFunc2(a) {
-	if(a){
-		return false;
-	}else {
-		return true;
-	}
-}
 
 function notFunc(a) {
     return (a ? false : true);
 }
 
-
 /// Arithmetic Operators
 var carryOut;
 var carryIn;
 var sum;
-
-function halfAdder2(a, b) {
-    if(andFunc(a,b)){
-        carryOut = true;
-        sum = false;
-    }else {
-        carryOut = false;
-        sum = false;
-    }
-    if(orFunc(a, b)){
-        carryOut = false;
-        sum = true;
-    }
-}
 
 function halfAdder(a, b) {
     return (andFunc(a,b) ? (
@@ -74,23 +25,6 @@ function halfAdder(a, b) {
             carryOut=false, sum=false
         )
     );
-}
-
-function fullAdder2(a, b, c) { // c: carry in
-    if(c){
-        if(andFunc(a, b)) {
-            carryOut = true;
-            sum = true;
-        }else if(orFunc(a, b)){
-            carryOut = true;
-            sum = false; 
-        }else {
-            carryOut = false; 
-            sum = true; 
-        }
-    }else {
-        return halfAdder(a,b);
-    }
 }
 
 function fullAdder(a, b, c) {
